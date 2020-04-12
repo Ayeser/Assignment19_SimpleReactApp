b338150541c48fe5a7ee08121218b96335cf7f68
Matthew Wester <MatthewWester@live.com>
2020-04-11 22:53:10 -0700

    Updates

---
+++ precache-manifest.5a88b069af17a207d0ef884a8fafb221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eb8e7daeba94faa03acae5d0832fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c83275e9d6fc817eb8",
+    "url": "/static/css/main.ef2f3ef7.chunk.css"
+  },
+  {
+    "revision": "a3753732af4ea99dc8db",
+    "url": "/static/js/2.2fcb9c18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2fcb9c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c83275e9d6fc817eb8",
+    "url": "/static/js/main.359c3932.chunk.js"
+  },
+  {
+    "revision": "457698f1677daef0ae8c",
+    "url": "/static/js/runtime-main.7d861018.js"
+  }
+]);
